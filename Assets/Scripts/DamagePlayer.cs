710adaecc318d3ae46b71f45c63b84f56a107255
KID <50666207+KID421@users.noreply.github.com>
2023-08-01 21:31:35 +0800

    Update 添加敵人攻擊系統

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -5,4 +5,15 @@ public class DamagePlayer : DamageBasic
 {
     [Header("血條")]
     public Image imgHp;
+
+    //private void Start()
+    //{
+    //    Damage(50);
+    //}
+
+    public override void Damage(float damage)
+    {
+        base.Damage(damage);
+        imgHp.fillAmount = hp / hpMax;
+    }
 }
