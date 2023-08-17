e656ed496418142b7e6b9fb969a61c73efc2ca90
KID <50666207+KID421@users.noreply.github.com>
2023-08-17 18:57:11 +0800

    Update 添加敵人生成

--- Assets/Scripts/SpawnEnemyManager.cs
+++ Assets/Scripts/SpawnEnemyManager.cs
@@ -6,4 +6,32 @@ public class SpawnEnemyManager : MonoBehaviour
     public SpawnSystem[] spawnSystems;
     [Header("生成怪物資料")]
     public DataSpawnEnemy[] dataSpawnEnemys;
+
+    private float timer;
+    private int index;
+
+    private void Update()
+    {
+        ChangeSpawnEnemy();
+    }
+
+    private void ChangeSpawnEnemy()
+    {
+        timer += Time.deltaTime;
+
+        if (index >= dataSpawnEnemys.Length) return;
+
+        if (timer >= dataSpawnEnemys[index].timeToSpawn)
+        {
+
+            for (int i = 0; i < spawnSystems.Length; i++)
+            {
+                spawnSystems[i].prefabEnemy = dataSpawnEnemys[index].prefabEnemy;
+                spawnSystems[i].interval = dataSpawnEnemys[index].intervalSpawn;
+            }
+
+            index++;
+            print("生成的波數：" + index);
+        }
+    }
 }
